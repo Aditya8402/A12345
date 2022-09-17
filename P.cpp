f08a5e3d4b5d6817f5d76719fc4c0e13a61e8dbc
Aditya8402 <85897393+Aditya8402@users.noreply.github.com>
2022-09-17 12:44:28 +0530

    Update P.cpp

--- P.cpp
+++ P.cpp
@@ -45,4 +45,46 @@ void Mails(usrname)
   }
   
 }
+void login(string username , password){
+ if(user[username]!=user.end())
+  {if(user[username]== password){
+    return true;
+  }  
+  }
+  return false;
+}
+void display(){
+    switch(test){
+        case 1:spam(username);
+        break;
+        case 2:drafts(username);
+        break;
+        case 3:unread(username);
+        break;
+        case 4:starred(username);
+        break;
+        case 5:inbox(username);
+        default : return;
+    }
+}
 
+void inbox(auto obj)
+{
+	showList(obj.inboxlist);
+}
+void starred(auto obj)
+{
+	showList(obj.starredlist);
+}
+void spam(auto obj)
+{
+	showList(obj.spamlist);
+}
+void drafts(auto obj)
+{
+	showList(obj.draftlist);
+}
+void unread(auto obj)
+{
+	showList(obj.unreadlist);
+}
